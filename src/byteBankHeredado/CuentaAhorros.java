eaddabe81cfe574f44be06f56e78eb9857705fa3
Gerson121295 <Gerson21millones@gmail.com>
2023-06-23 10:18:10 -0600

    2 Tratamiento de excepciones -Se agrego el proyecto byteBankHeredado de polimorfismo

---
+++ src/byteBankHeredado/CuentaAhorros.java
@@ -0,0 +1,16 @@
+package byteBankHeredado;
+
+public class CuentaAhorros extends Cuenta{
+
+    //Generate --Constructor
+    public CuentaAhorros(int agencia, int numero) {
+        super(agencia, numero);
+    }
+
+    @Override
+    public void depositar(double valor) {
+        this.saldo = this.saldo + valor;
+    }
+
+
+}
