16fd5a69d229ace8589a377b998b3d75f99f380e
Gerson121295 <Gerson21millones@gmail.com>
2023-06-23 22:30:49 -0600

    6 Finally y try with resources - Ejemplo Try With resources

--- src/java_pila_ejecucion/Conexion.java
+++ src/java_pila_ejecucion/Conexion.java
@@ -1,20 +1,25 @@
 package java_pila_ejecucion;
 
-public class Conexion {
+public class Conexion implements AutoCloseable { //se agrego interfaz AutoCloseable debido a la mejora ejemplo3 en TestConexion
 
     public Conexion(){//Constructor : abre la conexion cuando la creo.
         System.out.println("Abriendo conexion");
-        throw new IllegalStateException(); //se puede mandar la exception en el constructor
+        //throw new IllegalStateException(); //se puede mandar la exception en el constructor
         //La bomba esta explotando al momento de la creacion del objeto por lo que no creará la conexion.
     }
 
     public void leerDatos(){
         System.out.println("Recibiendo datos");
-        //throw new IllegalStateException(); //Exeption cuando la conexion a la BD no fue exitosa.
+        throw new IllegalStateException(); //Exeption cuando la conexion a la BD no fue exitosa.
     }
 
     public void cerrar(){
         System.out.println("Cerrando conexión");
     }
 
+
+    @Override
+    public void close() throws Exception { //metodo de la interface AutoCloseable
+        cerrar();
+    }
 }
--- src/java_pila_ejecucion/TestConexion.java
+++ src/java_pila_ejecucion/TestConexion.java
@@ -2,7 +2,7 @@ package java_pila_ejecucion;
 
 public class TestConexion {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception { //se agrego throws Exception debido al ejemplo3
 //Ejemplo 1.
         /*
         Conexion con = new Conexion();
@@ -21,6 +21,8 @@ public class TestConexion {
 //Finally es opcional no es obligatorio y solo puede haber un finally. try si debe llevar obligado un catch o un finally
 // try no puede ir solo.
 
+        //Ejemplo 2:
+        /*
         Conexion con = null;
         try {
             con = new Conexion();//La bomba esta explotando al momento de la creacion del objeto por lo que no creará la conexion.
@@ -34,9 +36,30 @@ public class TestConexion {
             if(con != null){
                 con.cerrar();
             }
+        }
+        */
 
+        //Ejemplo3: Mejora del codigo anterior: Try with resources
+    //Try with resources: inicializa un objeto pero este objeto debe implementar la interface
+    //AutoCloseable, asi tambien debe implementar su metodos close de AutoCloseable,
+    //
+//Este codigo hace lo mismo que los anteriores pero mas resumido.
+        try(Conexion con = new Conexion()){ //declarado la creacion de un objeto de tipo conexion dentro del try.
+                con.leerDatos();
+        }catch (IllegalStateException ex){
+            System.out.println("Ejecutando catch");
+            ex.printStackTrace();
         }
 
     }
 
 }
+
+//Cual usar: Si necesita que el objeto Conexion haga algo al momento de dejar de existir
+//cuando sea cerrado, cuando ya no se utilizará mas, si fuera un recurso que necesito asegurarme
+// de cerrarlo despues puedo implementar la interfaz AutoCloseable y el cerrará ese recurso.
+//Y va a ejecutar todo normalmente. Pero si mi objeto no implementa AutoCloseable utilizar las
+//anteriores.
+//Try no puede vivir solo necesita de  un catch o un finally o puede tener ambos.
+
+
