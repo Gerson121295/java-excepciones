39f9d950e0d2435d3b7c91ca9e8d525539f8646d
Gerson121295 <Gerson21millones@gmail.com>
2023-06-23 20:17:48 -0600

    5 Aplicando Excepciones - Overview: Checked y unchecked

--- src/java_pila_ejecucion/Cuenta.java
+++ src/java_pila_ejecucion/Cuenta.java
@@ -1,4 +1,7 @@
 package java_pila_ejecucion;
 
 public class Cuenta {
+    void deposita() throws MiException{ //throws obligo al metodo a verificar la exception
+
+    }
 }
--- src/java_pila_ejecucion/MiException.java
+++ src/java_pila_ejecucion/MiException.java
@@ -1,7 +1,7 @@
 package java_pila_ejecucion;
 
-//public class MiException extends RuntimeException{
- public class MiException extends Exception{
+//public class MiException extends RuntimeException{ //RuntimeException exception no verificada.
+ public class MiException extends Exception{ //Exception exception verificada. Por lo que necesitamos atrapar la exception
     public MiException(){
         super(); //para llamar al constructor del padre
     }
