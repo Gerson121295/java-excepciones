395aedd5f1cc4694c1ad60c71d9edd4780539f9b
Gerson121295 <Gerson21millones@gmail.com>
2023-06-23 22:04:22 -0600

    6 Finally y try with resources - Aplicando finally

--- src/java_pila_ejecucion/Conexion.java
+++ src/java_pila_ejecucion/Conexion.java
@@ -4,11 +4,13 @@ public class Conexion {
 
     public Conexion(){//Constructor : abre la conexion cuando la creo.
         System.out.println("Abriendo conexion");
+        throw new IllegalStateException(); //se puede mandar la exception en el constructor
+        //La bomba esta explotando al momento de la creacion del objeto por lo que no creará la conexion.
     }
 
     public void leerDatos(){
         System.out.println("Recibiendo datos");
-        throw new IllegalStateException(); //Exeption cuando la conexion a la BD no fue exitosa.
+        //throw new IllegalStateException(); //Exeption cuando la conexion a la BD no fue exitosa.
     }
 
     public void cerrar(){
--- src/java_pila_ejecucion/TestConexion.java
+++ src/java_pila_ejecucion/TestConexion.java
@@ -3,7 +3,8 @@ package java_pila_ejecucion;
 public class TestConexion {
 
     public static void main(String[] args) {
-
+//Ejemplo 1.
+        /*
         Conexion con = new Conexion();
         try {
             con.leerDatos();
@@ -15,8 +16,26 @@ public class TestConexion {
             System.out.println("Ejecutando Finally");
             con.cerrar();
         }
-
+*/
 //Toda conexion que abrimos debemos cerrarla. Podemos usar finally para asegurarnos de cerrar la conexion.
+//Finally es opcional no es obligatorio y solo puede haber un finally. try si debe llevar obligado un catch o un finally
+// try no puede ir solo.
+
+        Conexion con = null;
+        try {
+            con = new Conexion();//La bomba esta explotando al momento de la creacion del objeto por lo que no creará la conexion.
+            con.leerDatos();
+        }catch (IllegalStateException ex){
+            System.out.println("Capturando exception");
+            ex.printStackTrace();
+
+        }finally { //De una u otra forma. Hay o no halla error se ejecuta el metodo.
+            System.out.println("Ejecutando Finally");
+            if(con != null){
+                con.cerrar();
+            }
+
+        }
 
     }
 
